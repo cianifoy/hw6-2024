84c836e06826e1f5e8162ff6fe2891bbaa05a755
cianifoy <cfoy@umich.edu>
2024-11-22 19:42:46 -0500

    Add files via upload

--- js/video.js
+++ js/video.js
@@ -29,6 +29,55 @@ document.querySelector("#slower").addEventListener("click", function(){
     console.log("Speed is", video.playbackRate)
 });
 
+document.querySelector("#faster").addEventListener("click", function(){
+    console.log("speed up video")
+    video.playbackRate *= 1.10
+    console.log("speed is", video.playbackRate)
+});
+
+document.querySelector("#skip").addEventListener("click", function() {
+    console.log("skip video")
+    video.currentTime += 10;
+    console.log("time of video is", video.currentTime)
+});
+
+document.querySelector("#player1").addEventListener("ended", function() {
+    video.currentTime = 0;  
+    video.play();  
+    console.log("video has started over")
+});
+
+document.querySelector("#mute").addEventListener("click", function() {
+    video.muted = !video.muted;
+
+    if (video.muted) {
+        console.log("Video is muted");
+    } else {
+        console.log("Video is unmuted");
+    }
+});
+
+document.querySelector("#slider").addEventListener("input", function() {
+    video.volume = this.value / 100;  
+    console.log("Volume has been adjusted to " + video.volume * 100 + "%");
+    document.querySelector("#volume").innerHTML = video.volume * 100 + "%";
+});
+
+document.querySelector("#vintage").addEventListener("click", function() {
+    document.querySelector("#player1").classList.toggle("oldSchool");
+    console.log("styling has changed")
+
+});
+
+document.querySelector("#orig").addEventListener("click", function() {
+    video.classList.remove("oldSchool");
+    console.log("styling is back to original")
+
+});
+
+
+
+
 
 
 
